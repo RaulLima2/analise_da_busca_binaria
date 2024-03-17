dc9f5761030056d04ed624fcc237edf199d7647f
raullima2 <raulfotografialima@gmail.com>
2024-03-17 13:28:31 -0300

    fix: correção da busca binaria

--- src/busca_binaria.py
+++ src/busca_binaria.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def busca_binaria(chave:int, esquerda:int, direita:int, vetor:np.ndarray)->int:
+def busca_binaria(chave:int, vetor:np.ndarray, esquerda:int, direita:int) -> int:
+  esquerda:int = 0
+  direita:int = len(vetor) - 1
+  while direita >= esquerda:
     meio:int = int((esquerda + direita)//2)
-
-    if direita >= esquerda:
-        if vetor[meio] > chave:
-            return busca_binaria(chave, vetor, 0, meio - 1)
-        elif vetor[meio] < chave:
-            return busca_binaria(chave, vetor, meio + 1, len(vetor))
-        elif vetor[meio] == chave:
-            return meio
-    else:
-        return 0
+    if vetor[meio] == chave:
+      return meio
+    elif vetor[meio] > chave:
+      direita:int = meio - 1
+    elif vetor[meio] < chave:
+      esquerda:int = meio + 1
+  return -1
