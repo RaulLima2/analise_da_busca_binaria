fb479c38c673dd363a617a1416bd96bd7f57f143
raullima2 <raulfotografialima@gmail.com>
2024-03-22 08:58:05 -0300

    fix: busca_sequencial_otimizada.py

--- src/busca_sequencial_otimizada.py
+++ src/busca_sequencial_otimizada.py
@@ -2,8 +2,7 @@ import numpy as np
 
 def busca_sequencial_otimizada(chave:int, vetor:np.ndarray)->int:
     for index, item in enumerate(vetor):
-        if item <= chave:
-            if item == chave:
-                return index
-            elif item > chave:
-                return 0
+        if item < chave:
+            return -1
+        elif item == chave:
+            return index
