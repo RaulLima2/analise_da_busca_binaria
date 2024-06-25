39a8bc4db674471c462cb34dc24a5c3023e33cdc
raullima2 <raulfotografialima@gmail.com>
2024-06-25 15:07:06 -0300

    fix: fixed files in src, add docstring and fixed indentations

--- src/busca_sequencial.py
+++ src/busca_sequencial.py
@@ -1,7 +1,20 @@
+"""
+This module contains a function for performing a sequential search in an array.
+"""
 import numpy as np
 
 def busca_sequencial(chave:int, vetor:np.ndarray)->int:
-	for index, item in enumerate(vetor):
-		if item == chave:
-			return index
-	return -1
+    """
+    Performs a sequential search for a given key in an array.
+
+    Parameters:
+    chave (int): The key to search for.
+    vetor (np.ndarray): The array to search in.
+
+    Returns:
+    int: The index of the key in the array if found, otherwise -1.
+    """
+    for index, item in enumerate(vetor):
+        if item == chave:
+            return index
+    return -1
--- src/busca_sequencial_otimizada.py
+++ src/busca_sequencial_otimizada.py
@@ -1,9 +1,25 @@
+"""
+This module contains a function for performing an optimized sequential search on a given array.
+"""
 import numpy as np
 
-def busca_sequencial_otimizada(chave:int, vetor:np.ndarray)->int:
-    for index, item in enumerate(vetor):
-        if item > chave:
+
+def busca_sequencial_otimizada(vetor: np.ndarray, chave: int, inicio: int, final: int) -> int:
+    """
+    Perform an optimized sequential search on a given array.
+
+    Args:
+        vetor (np.ndarray): The array to be searched.
+        chave (int): The value to be searched for.
+        inicio (int): The starting index of the search range.
+        final (int): The ending index of the search range.
+
+    Returns:
+        int: The index of the first occurrence of the value in the array, or -1 if not found.
+    """
+    for index in range(inicio, final):
+        if vetor[index] > chave:
             break
-        if item == chave:
+        if vetor[index] == chave:
             return index
     return -1
