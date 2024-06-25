b6f3428b88d14050daaef68d4636b7f636714420
raullima2 <raulfotografialima@gmail.com>
2024-06-25 13:59:19 -0300

    update: updated line 7 in busca_sequencial_otimizada basead in complex ciclomatic

--- src/busca_sequencial_otimizada.py
+++ src/busca_sequencial_otimizada.py
@@ -4,6 +4,6 @@ def busca_sequencial_otimizada(chave:int, vetor:np.ndarray)->int:
     for index, item in enumerate(vetor):
         if item > chave:
             break
-        elif item == chave:
+        if item == chave:
             return index
     return -1
